e450731fe7c6d179f6dbbc3066179391566b6ca9
unknown <danielmedeiros52@gmail.com>
2017-12-07 15:30:57 -0300

    sidenavBar-com1

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -6,6 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  clicou2(cadastro){
+    if(cadastro.value==true){
+    cadastro.value=false
+    
+    return true 
+    }
+  else{
+    cadastro.value=true
+    return false
+  } 
+  }
+
+  conteudo(itemDoMenu){
+if (true){
+
+  alert(itemDoMenu.value)
+
+}
+
+  }
 
   constructor() { }
 
