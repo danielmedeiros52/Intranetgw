8f91b6c795b52177f8423e2580f4ff97fc72dab6
unknown <danielmedeiros52@gmail.com>
2017-12-11 18:01:39 -0300

    rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
