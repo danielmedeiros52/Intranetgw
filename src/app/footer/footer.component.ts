cfe66e1d61e5fc653be129320f19829e5b74f2ae
unknown <danielmedeiros52@gmail.com>
2017-12-19 15:22:39 -0300

    animations 2.1

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit} from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style,  animate,query,transition,stagger,keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-footer' ,
@@ -7,14 +7,17 @@ import { trigger, state, style, transition, animate } from '@angular/core';
   styleUrls: ['./footer.component.css'],
   animations: [
     trigger('movetolefet', [
-      state('in', style({transform: 'translateX(0)'})),
-      transition('void => *', [
-        style({transform: 'translateX(-100%)'}),
-        animate(100)
-      ]),
-      transition('* => void', [
-        animate(100, style({transform: 'translateX(100%)'}))
-      ])
+      
+    transition('*=>*',[
+      query(':enter',style({opacity:0}),{optional:true}),
+      query(':enter',stagger('300ms',[
+        animate('1s ease-in',keyframes([
+          style({opacity:0,transform:'translateY(-75px)',offset:0}),
+          style({opacity:.5,transform:'translateY(35px)',offset:.30}),
+          style({opacity:1,transform:'translateY(0)',offset:1}),
+        ]))
+      ]))
+    ])
     ])
   ]
 })
--- src/app/grupo-cliente-fornecedores/grupo-cliente-fornecedores.component.ts
+++ src/app/grupo-cliente-fornecedores/grupo-cliente-fornecedores.component.ts
@@ -1,9 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { trigger, state, style,  animate,query,transition,stagger,keyframes } from '@angular/animations';
 @Component({
   selector: 'app-grupo-cliente-fornecedores',
   templateUrl: './grupo-cliente-fornecedores.component.html',
-  styleUrls: ['./grupo-cliente-fornecedores.component.css']
+  styleUrls: ['./grupo-cliente-fornecedores.component.css'],
+  animations: [
+    trigger('dropdowngp', [
+      
+    transition('*=>*',[
+      query(':enter',style({opacity:0}),{optional:true}),
+      query(':enter',stagger('300ms',[
+        animate('1s ease-in',keyframes([
+          style({opacity:0,transform:'translateY(-75px)',offset:0}),
+          style({opacity:.5,transform:'translateY(25px)',offset:.20}),
+          style({opacity:1,transform:'translateY(0)',offset:1}),
+        ]))
+      ]))
+    ])
+    ])
+  ]
 })
 
 export class GrupoClienteFornecedoresComponent implements OnInit {
